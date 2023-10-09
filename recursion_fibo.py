e9a49bcd84d50630a4d542b5fbb9eb65f3d1c954
Lofile <79859770+kimjaehyun01@users.noreply.github.com>
2023-10-10 01:18:51 +0900

    Update recursion_fibo.py

--- recursion_fibo.py
+++ recursion_fibo.py
@@ -1,9 +1,11 @@
+#chap3 재귀함수
+#재귀함수로 피보나치수열 구현하기
 def fib(n):
-    if n == 0:
+    if n == 0:                #기본단계1
         return 0
-    elif n == 1 or n == 2:
+    elif n == 1 or n == 2:    #기본단계2
         return 1
-    else:
+    else:                    #재귀단계
         return fib(n - 1) + fib(n - 2)
 
 for i in range(1,11):
